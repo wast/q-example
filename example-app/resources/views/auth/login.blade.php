cdb8200357b8473554f25d51dd15e4e197ef06df
Stjepan Majdak <stjepan.majdak@a1.hr>
2022-07-01 16:01:39 +0200

    Remove Remember Me

--- example-app/resources/views/auth/login.blade.php
+++ example-app/resources/views/auth/login.blade.php
@@ -32,14 +32,6 @@
                                 required autocomplete="current-password" />
             </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
             <div class="flex items-center justify-end mt-4">
                 <x-button class="ml-3">
                     {{ __('Log in') }}
